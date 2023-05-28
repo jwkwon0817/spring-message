9c2e33524e6e78f0ceb18fa8fe32914fd8683ded
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-28 21:07:19 +0900

    Test Data Init

--- src/main/java/com/codemos/springmessage/TestDataInit.java
+++ src/main/java/com/codemos/springmessage/TestDataInit.java
@@ -9,16 +9,14 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class TestDataInit {
-
-    private final ItemRepository itemRepository;
-
-    /**
-     * Add Data for Test
-     */
-    @PostConstruct
-    public void init() {
-        itemRepository.save(new Item("itemA", 10000, 10));
-        itemRepository.save(new Item("itemB", 20000, 20));
-    }
-
+	private final ItemRepository itemRepository;
+	
+	/**
+	 * Add Data for Test
+	 */
+	@PostConstruct
+	public void init() {
+		itemRepository.save(new Item("itemA", 10000, 10));
+		itemRepository.save(new Item("itemB", 20000, 20));
+	}
 }
