9758c7663e7b50067851be909e0d3a9dcca67524
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-28 20:14:27 +0900

    SpringMessage Application Tests

---
+++ src/test/java/com/codemos/springmessage/SpringMessageApplicationTests.java
@@ -0,0 +1,11 @@
+package com.codemos.springmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessageApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
